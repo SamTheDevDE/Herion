e768055c25a60b3350a09dd8ca98926478034d10
SamTheDevDE <sdomke@samthedev.de>
2026-04-23 23:38:28 +0200

    Bump Versions and update to prisma v7

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -1,9 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import Logger from './Logger'
+import Logger from './Logger';
 
 class Database {
     private static _instance: PrismaClient | null = null;
     private _log: Logger;
+    private _adapter: any;
 
     public constructor(logger: Logger) {
         this._log = logger;
@@ -14,7 +16,8 @@ class Database {
         // checks if there is already an instance of "database" if not make one
         if (!Database._instance) {
             try {
-                Database._instance = new PrismaClient();
+                this._adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+                Database._instance = new PrismaClient({ adapter: this._adapter });
                 Database._instance.$connect()
                     .then(() => this._log.info('Prisma client connected successfully.'))
                     .catch((err: any) => this._log.error('Error connecting to Prisma client:', err));
